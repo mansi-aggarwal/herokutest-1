86053dad1e9827390ef81e1e2c7f1baf66b86073
anantsingh11 <51476103+anantsingh11@users.noreply.github.com>
2019-06-28 13:21:21 -0400

    Update anant.py

--- anant.py
+++ anant.py
@@ -1,1 +1,19 @@
+import socket
 
+serv = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+serv.bind(('127.0.0.2',8080))
+serv.listen(5)
+
+while True:
+  conn, addr = serv.accept()from client=''
+  while True:
+    data = conn.recv(4096) 
+    if not data: break
+      from_client += data
+      print from_client
+      
+      conn.send("I am SERVER\n")
+    
+conn.close()
+print'client disconnected'
